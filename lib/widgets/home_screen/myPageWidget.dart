11f7ce87e47b4e434097025df6b4ba81679d306a
Nagi Mohamed <117310405+nagiElshershaby@users.noreply.github.com>
2023-10-20 00:09:11 +0300

    adding a magnifier widget

--- lib/widgets/home_screen/myPageWidget.dart
+++ lib/widgets/home_screen/myPageWidget.dart
@@ -1,7 +1,7 @@
-
 import 'package:flutter/material.dart';
 
-Widget myGradientWidget(BuildContext context,String image, int index, int length) {
+Widget myGradientWidget(
+    BuildContext context, String image, int index, int length) {
   return Container(
     width: MediaQuery.of(context).size.width,
     height: MediaQuery.of(context).size.height,
@@ -15,10 +15,38 @@ Widget myGradientWidget(BuildContext context,String image, int index, int length
         ],
       ),
     ),
-    child: Image.asset(
+    child: magnifier(Image.asset(
       image,
       fit: BoxFit.fill,
       filterQuality: FilterQuality.high,
-    ),
+    )),
+  );
+}
+
+
+Widget magnifier(Widget child){
+  return Stack(
+    children: [
+      InteractiveViewer(
+        boundaryMargin: EdgeInsets.all(20),
+        child: child,
+      ),
+      // const Align(
+      //   alignment: Alignment.center,
+      //   child: RawMagnifier(
+      //     size: Size(200, 100),
+      //     decoration: MagnifierDecoration(
+      //       shape: OutlineInputBorder(
+      //         borderSide: BorderSide(color: Colors.deepOrange, width: 2),
+      //         borderRadius: BorderRadius.all(
+      //           Radius.circular(20),
+      //         ),
+      //       ),
+      //     ),
+      //     focalPointOffset: Offset.zero,
+      //     magnificationScale: 2,
+      //   ),
+      // ),
+    ],
   );
 }
